20c3196beb36d217b3da2c536082eae05d13a4bc
BelialClover <67656448+BelialClover@users.noreply.github.com>
2023-06-16 23:59:28 -0500

    Made a different page for the mystery gifts

--- plugins/mysterygift.py
+++ plugins/mysterygift.py
@@ -1,5 +1,4 @@
 # echo-client.py
-
 import socket
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
@@ -8,7 +7,6 @@ PORT = 8888  # The port used by the server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     s.send(b'8')
-    #s.sendall(b"8")
     data = s.recv(1024)
 
-print(f"Received {data!r}")
+print(f"received {data!r}")
