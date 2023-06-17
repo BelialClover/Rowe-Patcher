7d7affa6c80f5e156f1e149b45bcd012ec466b48
BelialClover <67656448+BelialClover@users.noreply.github.com>
2023-06-16 22:50:54 -0500

    Updated mystery gift

--- plugins/mysterygift.py
+++ plugins/mysterygift.py
@@ -7,7 +7,7 @@ PORT = 8888  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.send(b'2')
+    s.send(b'8')
     #s.sendall(b"8")
     data = s.recv(1024)
 
